fb72e81316e0c9b70e6206c0267a1e1a92ef9b8f
geges1101 <62469977+geges1101@users.noreply.github.com>
2022-11-07 04:35:18 +0300

    Add files via upload

--- go-balance-manager/cmd/main/main.go
+++ go-balance-manager/cmd/main/main.go
@@ -1,16 +1,17 @@
 package main
 
-import(
+import (
 	"log"
 	"net/http"
+
+	"github.com/geges1101/go-balance-manager/pkg/routes"
 	"github.com/gorilla/mux"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
-	"github.com/geges1101/go-balance-manager/pkg/routes"
 )
 
-func main(){
+func main() {
 	r := mux.NewRouter()
 	routes.RegisterBalanceRoutes(r)
 	http.Handle("/", r)
-	log.Fatal(http.ListenAndServe("localhost:9010", r))
+	log.Fatal(http.ListenAndServe("localhost:3306", r))
 }
